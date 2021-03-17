c82f17753006e108c0ce49da3454495329e4783b
Cosmin James <cosminjames555@gmail.com>
2021-03-17 18:12:42 +0200

    began virtual memory mapper

--- kernel/include/kernel/system/memory.h
+++ kernel/include/kernel/system/memory.h
@@ -1,7 +1,26 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stdint.h>
+
+typedef enum Perms {
+    Perm_None = 0b000,
+    Perm_X =    0b001,
+    Perm_W =    0b010,
+    Perm_R =    0b100,
+
+    Perm_RX =   0b011,
+    Perm_RW =   0b110,
+    Perm_RWX =  0b111
+} Perms;
+
+typedef enum MemoryType {
+    MemoryType_Memory,
+    MemoryType_MMIO
+} MemoryType;
+
 void init_paging();
+void map(uint64_t vaddr, uint64_t paddr, uint64_t size, Perms perm, MemoryType mt);
 
 void *kallocFrame();
 void kfreeFrame(void *frame);
--- kernel/kernel/system/memory/paging.c
+++ kernel/kernel/system/memory/paging.c
@@ -5,10 +5,11 @@
 
 #include <kernel/kdebug.h>
 
+#define PAGE_SIZE 4096
 #define ENTRY_COUNT 512
 
 extern void enablePAE();
-void loadPageDirectory(uint64_t *page_dir);
+static void loadPageDirectory(uint64_t *page_dir);
 extern void enablePaging();
 
 uint64_t page_dir_ptr_tab[4] __attribute__((aligned(0x20)));
@@ -25,11 +26,8 @@ void init_paging() {
     for (size_t i = 0; i < ENTRY_COUNT; i++)
         first_page_table[i] = (i * 0x1000) | 0b11;
 
-    // Paging tutorial said "put first page table in the directory"...
-    //page_dir[0] = ((uint64_t)first_page_table) | 0b11;
-
-    page_dir[0] = 0b10000011; // ...but PAE paging tutorial said do this?!
-    page_dir_ptr_tab[0] = (uint64_t)&page_dir | 1;
+    page_dir[0] = ((uint64_t)first_page_table) | 0b11;
+    page_dir_ptr_tab[0] = (uint64_t)page_dir | 1;
 
     klog_debug("%p\n", (void*)page_dir_ptr_tab);
 
@@ -40,4 +38,63 @@ void init_paging() {
 
 void loadPageDirectory(uint64_t *page_dir) {
     asm volatile ("mov cr3, %0" :: "r" (page_dir));
+}
+
+/*********************************
+ *   WARNING! WORK IN PROGRESS   *
+ *********************************/
+
+void map(uint64_t vaddr, uint64_t paddr, uint64_t size, Perms perm, MemoryType mt) {
+    
+    size += PAGE_SIZE - 1;
+    size &= ~(PAGE_SIZE - 1);
+
+    //var root: table_ptr = cr3;
+
+    //const levels = 4
+
+    //const base_bits = @intCast(u6, base_bits);
+
+    //const small_bits = extra_bits(perm, mt, page_size, true);
+    //const large_bits = extra_bits(perm, mt, page_size, false);
+
+    //while(size != 0) {
+    //    var current_step_size = page_size << @intCast(u6, (base_bits - 3) * (levels - 1));
+    //    var level = levels - 1;
+    //    var current_table = root;
+
+    //    while(true) {
+    //        const ind = get_index(vaddr, base_bits, level);
+    //        // We can only map at this level if it's not a table
+    //        switch(decode(current_table[ind], level == 0)) {
+    //            .Mapping => {
+    //                sabaton.log_hex("Overlapping mapping at ", vaddr);
+    //                sabaton.log_hex("PTE is ", current_table[ind]);
+    //                unreachable;
+    //            },
+    //            .Table => { }, // Just iterate to the next level
+    //            .Empty => {
+    //                // If we can map at this level, do so
+    //                if(can_map(size, vaddr, paddr, current_step_size)) {
+    //                    const bits = if(level == 0) small_bits else large_bits;
+    //                    make_mapping_at(&current_table[ind], paddr, bits);
+    //                    //sabaton.log_hex("At idx ", ind);
+    //                    break;
+    //                }
+    //                // Otherwise, just iterate to the next level
+    //            }
+    //        }
+
+    //        if(level == 0)
+    //            unreachable;
+
+    //        current_table = make_table_at(&current_table[ind]);
+    //        current_step_size >>= (base_bits - 3);
+    //        level -= 1;
+    //    }
+
+    //    vaddr += current_step_size;
+    //    paddr += current_step_size;
+    //    size -= current_step_size;
+    //}
 }
