dedc76a3f08198c0b3236c4ccafeb96245ee7b88
Cosmin James <cosminjames555@gmail.com>
2021-04-15 13:46:49 +0300

    Added utility functions and types

--- kernel/include/kernel/system/memory.h
+++ kernel/include/kernel/system/memory.h
@@ -2,6 +2,11 @@
 #define MEMORY_H
 
 #include <stdint.h>
+#include <stdbool.h>
+
+#define KiB(N) ((uint64_t)(N) << 10)
+#define MiB(N) ((uint64_t)(N) << 20)
+#define GiB(N) ((uint64_t)(N) << 30)
 
 typedef enum Perms {
     Perm_None = 0b000,
@@ -19,8 +24,20 @@ typedef enum MemoryType {
     MemoryType_MMIO
 } MemoryType;
 
+typedef enum Decoded {
+    Decoded_Empty,
+    Decoded_Mapping,
+    Decoded_Table
+} Decoded;
+
+extern const uint64_t higher_base;
+extern const uint64_t kernel_base;
+
 void init_paging();
 void map(uint64_t vaddr, uint64_t paddr, uint64_t size, Perms perm, MemoryType mt);
+Decoded decode(uint64_t entry, bool bottom_level);
+
+#define identity_map(ADDR, ...) map(ADDR, ADDR, __VA_ARGS__);
 
 void *kallocFrame();
 void kfreeFrame(void *frame);
