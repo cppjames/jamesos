0157a8e103dc62295426442424c2925f9a9452b1
Cosmin James <cosminjames555@gmail.com>
2021-04-15 13:50:33 +0300

    Removed unnecessary functions, improved assembly formatting

--- kernel/kernel/system/memory/paging.asm
+++ kernel/kernel/system/memory/paging.asm
@@ -1,35 +1,69 @@
-global enablePAE
-global enablePaging
+; Helper functions for enabling paging (mostly for changing
+; the contents of various control registers.
 
-enablePAE:
-    push rbp
-    mov rbp, rsp
+section .data:
 
-    ; Set bit 5 in CR4
-    mov rax, cr4
-    bts rax, 5
-    mov cr4, rax
+IA32_EFER_ID        equ     0xC0000080      ; MSR number of IA32_EFER
+IA32_EFER_NXE       equ     11              ; IA32_EFER.NXE (no execute) bit
 
-    mov rsp, rbp
-    pop rbp
+section .text:
+
+global loadTable
+global getTable
+global enableNXE
+
+; *********************************************************
+; Function to load the physical address of the PML4 into CR3.
+
+; Parameters:
+;     (qword) rdi - PML4 paddr
+
+; C prototype:
+;     void loadTable(uint64_t);
+
+loadTable:
+    mov cr3, rdi
     ret
 
-enablePaging:
-    push rbp
-    mov rbp, rsp
-    
-    ; Save CR0 in RAX
-    mov rax, cr0
-    
-    ; Set paging bit
-    push rbx
-    mov rbx, 0x80000000
-    or rax, rbx
-    pop rbx
-    
-    ; Load CR0 from RAX
-    mov cr0, rax
-    
-    mov rsp, rbp
-    pop rbp
+
+; *********************************************************
+; Function to get the physical address of the PML4 from CR3.
+
+; Returns:
+;     (qword) rax - PML4 paddr
+
+; C prototype:
+;     uint64_t getTable();
+
+getTable:
+    mov rax, cr3
+    ret
+
+
+; *********************************************************
+; Function to enable IA32_EFER.NXE (no-execute) bit. Only
+; needs to be called once.
+
+; C prototype:
+;     void enableNXE();
+
+enableNXE:
+    push rax
+    push rcx
+    push rdx
+
+    ; -----
+    ;  Read EFER in EDX:EAX
+    mov rcx, IA32_EFER_ID
+    rdmsr
+
+    ; Set IA32_EFER.NXE
+    bts eax, IA32_EFER_NXE
+
+    ; Write back into EFER from EDX:EAX
+    wrmsr
+
+    pop rdx
+    pop rcx
+    pop rax
     ret
