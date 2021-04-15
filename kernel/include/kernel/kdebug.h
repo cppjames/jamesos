800436c6201699a31debd8d318d555811c3e4b1d
Cosmin James <cosminjames555@gmail.com>
2021-04-15 13:45:54 +0300

    Added trivial runtime breakpoint function to kdebug.h

--- kernel/include/kernel/kdebug.h
+++ kernel/include/kernel/kdebug.h
@@ -9,10 +9,27 @@
 extern "C" {
 #endif
 
+#define STRINGIFY(X) #X
+#define STRGY(X) STRINGIFY(X)
+
+void kdebug_hexdump64(void *address, size_t count);
+void kdebug_hexdump32(void *address, size_t count);
+void kdebug_hexdump16(void *address, size_t count);
 void printch_port(char ch, __attribute__((unused)) void* arg);
 
 #define klog_debug(...) \
     fctprintf(printch_port, (void*)0, __VA_ARGS__)
+
+void _kbreak();
+
+#define kbreak(...)                            \
+    do {                                       \
+        printf("Function %s(): Line %s: [B] ", \
+               __func__, STRGY(__LINE__));     \
+        printf(__VA_ARGS__);                   \
+        putchar('\n');                         \
+        _kbreak();                             \
+    } while (0)
 
 #ifdef __cplusplus
 }
