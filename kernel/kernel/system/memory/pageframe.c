29417b866320d8f14bfa75de3ce1b09f8ae60f98
Cosmin James <cosminjames555@gmail.com>
2021-04-15 13:49:58 +0300

    Improved PMM, now setting allocated space to zero and using mapped vaddr for allocation base

--- kernel/kernel/system/memory/pageframe.c
+++ kernel/kernel/system/memory/pageframe.c
@@ -6,15 +6,15 @@
 
 #define FREE 0
 #define USED 1
-#define MAX_PAGES 1024
+#define PAGE_SIZE 4096
+#define MAX_PAGES 8192
 #define FRAME_BUFFER_SIZE 16
 
-extern uint32_t kernel_end;
 uint8_t frame_map[MAX_PAGES / 8 + 1] = { 0 };
 
 static inline void *getStartFrame() {
-    size_t kernel_end_address = (size_t)&kernel_end;
-    return (void*)((kernel_end_address & ~(size_t)0x0FFF) + 0x1000);
+    size_t kernel_end_address = 0xFFFF800000118000;
+    return (void*)((kernel_end_address & ~(size_t)0x0FFF) + PAGE_SIZE);
 }
 
 static inline bool getFrameStatus(size_t index) {
@@ -35,7 +35,7 @@ static void *kallocFrameFind()
     for (size_t i = 0; i < MAX_PAGES; i++) {
         if (getFrameStatus(i) == FREE) {
             setFrameStatus(i, USED);
-            return getStartFrame() + (i * 0x1000);
+            return getStartFrame() + (i * PAGE_SIZE);
         }
     }
 
@@ -51,13 +51,17 @@ void *kallocFrame() {
             frame_buffer[i] = kallocFrameFind();
 
     void *frame = frame_buffer[frame_index];
+
+    for (size_t i = 0; i < PAGE_SIZE; i++)
+        ((unsigned char*)frame)[i] = 0x0;
+
     frame_index = (frame_index + 1) % FRAME_BUFFER_SIZE;
     return frame;
 }
 
 void kfreeFrame(void *frame) {
     void *frame_offset = (void*)(frame - getStartFrame());
-    size_t index = (size_t)frame_offset / 0x1000;
+    size_t index = (size_t)frame_offset / PAGE_SIZE;
 
     setFrameStatus(index, FREE);
 }
