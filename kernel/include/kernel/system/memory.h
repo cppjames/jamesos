17a8645530937ab71d9a44e68a66b2af1da2d8a3
Cosmin James <cosminjames555@gmail.com>
2021-05-19 09:20:01 +0300

    Typedef'd some common types, improved formatting

--- kernel/include/kernel/system/memory.h
+++ kernel/include/kernel/system/memory.h
@@ -2,24 +2,24 @@
 #define MEMORY_H
 
 #include <stdint.h>
+#include <stddef.h>
 #include <stdbool.h>
 
-// TODO - Move these somewhere else
-#define KiB(N) ((uint64_t)(N) << 10)
-#define MiB(N) ((uint64_t)(N) << 20)
-#define GiB(N) ((uint64_t)(N) << 30)
-#define bit(N) (1UL << (N))
-#define firstBits(N) (bit(N) - 1UL)
+typedef uintptr_t Vaddr;
+typedef uintptr_t Paddr;
+
+typedef uint64_t Entry;
+typedef Entry *Table;
 
 typedef enum Perms {
     Perm_None = 0b000,
-    Perm_X =    0b001,
-    Perm_W =    0b010,
-    Perm_R =    0b100,
+    Perm_X    = 0b001,
+    Perm_W    = 0b010,
+    Perm_R    = 0b100,
 
-    Perm_RX =   0b011,
-    Perm_RW =   0b110,
-    Perm_RWX =  0b111
+    Perm_RX   = 0b011,
+    Perm_RW   = 0b110,
+    Perm_RWX  = 0b111
 } Perms;
 
 typedef enum MemoryType {
@@ -36,13 +36,21 @@ typedef enum Decoded {
 extern const uint64_t higher_base;
 extern const uint64_t kernel_base;
 
-void init_paging();
-void map(uint64_t vaddr, uint64_t paddr, uint64_t size, Perms perm, MemoryType mt);
-Decoded decode(uint64_t entry, bool bottom_level);
+// TODO - Move these somewhere else
+#define KiB(N) ((uint64_t)(N) << 10)
+#define MiB(N) ((uint64_t)(N) << 20)
+#define GiB(N) ((uint64_t)(N) << 30)
+#define bit(N) (1UL << (N))
+#define firstBits(N) (bit(N) - 1UL)
 
-#define identity_map(ADDR, args...) map(ADDR, ADDR, args);
+#define PAGE_SIZE KiB(4)
 
-void *kallocFrame();
-void kfreeFrame(void *frame);
+inline uint64_t alignUp(uint64_t addr, size_t size) {
+    return (addr + (size - 1)) & ~(size - 1);
+}
+
+inline uint64_t alignDown(uint64_t addr, size_t size) {
+    return (addr / size) * size;
+}
 
 #endif // MEMORY_H
