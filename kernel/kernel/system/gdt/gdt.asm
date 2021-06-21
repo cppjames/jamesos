9ca7f683bc0652f2e8a8b1e44708545c7e8bcd54
Cosmin James <cosminjames555@gmail.com>
2021-06-21 23:56:55 +0300

    Renamed gdt assembly file and added comments

---
+++ kernel/kernel/system/gdt/gdt.asm
@@ -0,0 +1,44 @@
+global reloadSegments
+global loadGdt
+
+; *********************************************************
+; Function to load the GDT pointer into GDTR.
+;
+; Parameters:
+;     (qword) rdi - the address of a GDT pointer structure
+;
+; C prototype:
+;     void loadGdt(GdtPtr *gdt_ptr);
+
+loadGdt:
+    lgdt [rdi]
+    ret
+
+
+; *********************************************************
+; Function to reload all segment registers after the GDT has been loaded.
+;
+; C prototype:
+;     void reloadSegments();
+
+reloadSegments:
+    ; Set up interrupts stack frame
+    mov rax, rsp
+    push 0x10
+    push rax
+    pushf
+    push 0x8
+    push reload_cs
+
+    ; Reload code segment by far-returning to reload_cs
+    iretq
+
+reload_cs:
+    ; Reload data segment registers
+    mov   ax, 0x10
+    mov   ds, ax
+    mov   es, ax
+    mov   fs, ax
+    mov   gs, ax
+    mov   ss, ax
+    ret
