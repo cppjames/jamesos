4020e1747a676f8f3d75c154da14a046217db06e
Cosmin James <cosminjames555@gmail.com>
2021-06-22 00:02:12 +0300

    Removed terrible endline layout

--- libc/sys/io.c
+++ libc/sys/io.c
@@ -5,15 +5,15 @@ uint8_t inb(uint16_t port)
 {
     uint8_t ret;
     asm volatile ("inb %0, %1"
-                  : "=a"(ret)
-                  : "Nd"(port));
+        : "=a"(ret)
+        : "Nd"(port));
     return ret;
 }
 
 void outb(uint16_t port, uint8_t value)
 {
     asm volatile ("outb %1, %0"
-                  :
-                  : "a"(value), "Nd"(port)
-                  :);
+        :
+        : "a"(value), "Nd"(port)
+        :);
 }
