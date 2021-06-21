912236d4f8a02e1cc4f7b9cf8faa9e4616543e23
Cosmin James <cosminjames555@gmail.com>
2021-06-21 23:54:32 +0300

    Added convenience color macros

--- kernel/kernel/shell/shell_internal.h
+++ kernel/kernel/shell/shell_internal.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdbool.h>
 
+#define COLOR_NORMAL    vgaEntryColor(VgaColor_LightGray, VgaColor_Black)
+#define COLOR_HIGHLIGHT vgaEntryColor(VgaColor_Black, VgaColor_LightGray)
+
 #define COMMAND_SIZE 4096
 
 void addCursorCharacter(unsigned char ch);
