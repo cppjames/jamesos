ab067e7d505a4630c2517a899b036a1247ae9b84
Cosmin James <cosminjames555@gmail.com>
2021-08-03 13:37:29 +0300

    Removed generic push/pop macro from file

--- kernel/kernel/system/exceptions/excepton_isr.asm
+++ kernel/kernel/system/exceptions/excepton_isr.asm
@@ -1,39 +1,3 @@
-%macro pushaq 0
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rbp
-    push rdi
-    push rsi
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-%endmacro
-
-%macro popaq 0
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsi
-    pop rdi
-    pop rbp
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-%endmacro
-
 ; Instructions for popping error informations off the stack
 %macro pop_err 0
     mov rdi, qword [rsp]
@@ -48,8 +12,8 @@
 ; Generic macro to handle an exception
 %macro handle_exc 2
     extern excHandler%1
-
     global excIsr%1
+    
     excIsr%1:
         pushaq              ; Save all registers
         %2                  ; To pop or not to pop
