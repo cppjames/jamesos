dfe0ed8b4b9e86252edccbd04b6844b8bdee8b3b
Cosmin James <cosminjames555@gmail.com>
2021-08-03 13:38:44 +0300

    Removed generic push/pop macro

--- kernel/kernel/system/idt/irq/irq_isr.asm
+++ kernel/kernel/system/idt/irq/irq_isr.asm
@@ -1,38 +1,4 @@
-%macro pushaq 0
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rbp
-    push rdi
-    push rsi
-    push r8
-    push r9
-    push r10
-    push r11
-    push r12
-    push r13
-    push r14
-    push r15
-%endmacro
-
-%macro popaq 0
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop r11
-    pop r10
-    pop r9
-    pop r8
-    pop rsi
-    pop rdi
-    pop rbp
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-%endmacro
+extern irqHandlers
 
 ; End of interrupt signal for master PIC IRQs.
 %macro eoi_pic_master 0
@@ -48,11 +14,22 @@
 %endmacro
 
 %macro handle_irq 2
-    extern irqHandler%1     ; Use external irqHandler C function
     global irqIsr%1         ; Declare irqIsr function as global
+    
     irqIsr%1:
         pushaq              ; Save registers
-        call irqHandler%1   ; Call irqHandler function
+
+        ; Get IRQ handler from lookup table
+        mov rcx, %1
+        mov rbx, qword [irqHandlers + rcx * 8]
+
+        ; If handler is NULL, skip
+        cmp rbx, 0
+        je missingHandler%1
+
+        call rbx
+    missingHandler%1:
+    
         %2                  ; Send end-of-interrupt signal
         popaq               ; Restore registers
         iretq               ; Return from interrupt
@@ -66,7 +43,13 @@
     handle_irq %1, eoi_pic_slave
 %endmacro
 
-handle_irq_master 0
+%macro handle_irq_no_eoi 1
+    handle_irq %1, nop
+%endmacro
+
+section .text
+
+handle_irq_no_eoi 0
 handle_irq_master 1
 handle_irq_master 2
 handle_irq_master 3
