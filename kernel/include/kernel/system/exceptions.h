98332dd5a9225eecbb7fc665d85e714aad84251f
Cosmin James <cosminjames555@gmail.com>
2021-08-03 13:33:15 +0300

    Updated function declarations

--- kernel/include/kernel/system/exceptions.h
+++ kernel/include/kernel/system/exceptions.h
@@ -5,27 +5,27 @@
 
 void exceptionHandler(uint64_t exc);
 
-void excIsr0();
-void excIsr1();
-void excIsr2();
-void excIsr3();
-void excIsr4();
-void excIsr5();
-void excIsr6();
-void excIsr7();
-void excIsr8();
-void excIsr9();
-void excIsr10();
-void excIsr11();
-void excIsr12();
-void excIsr13();
-void excIsr14();
-void excIsr15();
-void excIsr16();
-void excIsr17();
-void excIsr18();
-void excIsr19();
-void excIsr20();
-void excIsr30();
+void excIsr0(void);
+void excIsr1(void);
+void excIsr2(void);
+void excIsr3(void);
+void excIsr4(void);
+void excIsr5(void);
+void excIsr6(void);
+void excIsr7(void);
+void excIsr8(void);
+void excIsr9(void);
+void excIsr10(void);
+void excIsr11(void);
+void excIsr12(void);
+void excIsr13(void);
+void excIsr14(void);
+void excIsr15(void);
+void excIsr16(void);
+void excIsr17(void);
+void excIsr18(void);
+void excIsr19(void);
+void excIsr20(void);
+void excIsr30(void);
 
 #endif // EXCEPTIONS_H
