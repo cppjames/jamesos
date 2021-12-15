2149c8623bd2c6fb3c7dd224e5bfcdfff74a9595
Cosmin James <cosminjames555@gmail.com>
2021-12-15 16:50:28 +0200

    Made inline functions static inline

--- libc/include/ctype.h
+++ libc/include/ctype.h
@@ -10,27 +10,27 @@
 extern "C" {
 #endif
 
-inline bool isdigit(char c) {
+static inline bool isdigit(char c) {
 	return c >= '0' && c <= '9';
 }
 
-inline bool islower(char c) {
+static inline bool islower(char c) {
 	return c >= 'a' && c <= 'z';
 }
 
-inline bool isupper(char c) {
+static inline bool isupper(char c) {
 	return c >= 'A' && c <= 'Z';
 }
 
-inline bool isalpha(char c) {
+static inline bool isalpha(char c) {
 	return islower(c) || isupper(c);
 }
 
-inline bool isalnum(char c) {
+static inline bool isalnum(char c) {
 	return isdigit(c) || isalpha(c);
 }
 
-inline bool isspace(char c) {
+static inline bool isspace(char c) {
 	return c == ' ' 	||
 		   c == '\f'	||
 		   c == '\r'	||
@@ -38,37 +38,37 @@ inline bool isspace(char c) {
 		   c == '\v';
 }
 
-inline bool tolower(char c) {
+static inline bool tolower(char c) {
 	return isupper(c) ? (c + ('a' - 'A')) : c;
 }
 
-inline bool toupper(char c) {
+static inline bool toupper(char c) {
 	return islower(c) ? (c + ('A' - 'a')) : c;
 }
 
-inline bool iscntrl(char ch) {
+static inline bool iscntrl(char ch) {
 	return (ch >= 0x00 && ch <= 0x1F) || (ch == 0x7F);
 }
 
-inline bool isprint(char ch) {
+static inline bool isprint(char ch) {
 	return (ch >= 0x20 && ch <= 0x7E);
 }
 
-inline bool isgraph(char ch) {
+static inline bool isgraph(char ch) {
 	return (ch >= 0x21 && ch <= 0x7E);
 }
 
-inline bool isxdigit(char ch) {
+static inline bool isxdigit(char ch) {
     return (ch >= 'A' && ch <= 'F') ||
            (ch >= 'a' && ch <= 'f') ||
 	       isdigit(ch);
 }
 
-inline bool isblank(char ch) {
+static inline bool isblank(char ch) {
 	return (ch == ' ' || ch == '\t');
 }
 
-inline bool ispunct(char ch) {
+static inline bool ispunct(char ch) {
 	return (ch >= 0x21 && ch <= 0x2F) ||
 	       (ch >= 0x3A && ch <= 0x40) ||
 	       (ch >= 0x5B && ch <= 0x60) ||
