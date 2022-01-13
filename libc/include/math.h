1325b84ba388c17f684a16e674f16d228b315737
Cosmin James <cosminjames555@gmail.com>
2022-01-13 14:43:23 +0200

    Uncommented a few floating point math functions

--- libc/include/math.h
+++ libc/include/math.h
@@ -22,9 +22,6 @@ static inline int max(int n, int m) {
 	return (n > m) ? n : m;
 }
 
-/*
-	TODO: Support for floating point arithmetic.
-
 static inline double fabs(double x) {
 	return (x < 0) ? -x : x;
 }
@@ -35,7 +32,7 @@ static inline double fmin(double x, double y) {
 
 static inline double fmax(double x, double y) {
 	return (x > y) ? x : y;
-}*/
+}
 
 size_t ndigits(int, size_t);
 size_t zdigits(size_t, size_t);
